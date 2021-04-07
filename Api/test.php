84276ebeecee0a033cb1d206904ceb114b8fa39d
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2021-04-07 08:31:26 +0200

    add account

--- Api/test.php
+++ Api/test.php
@@ -4,26 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/OwnerDao.class.php";
-$owner_dao = new OwnerDao();
-//$owner = $owner_dao->get_owner_by_email("filipljoljic2000@gmail.com");
+require_once dirname(__FILE__)."/dao/AccountDao.class.php";
 
-$owner1 =[
-"Name"=>"Ahmed becirevic",
-"Age"=>"21",
-"email"=>"ahmed12345@gmail.com",
-"Address"=>"nedzarici nesto",
-"accountId"=>"21"
+$dao = new AccountDao();
 
+$dao->add_account([
+  "name" => "Filip Ljoljic",
+  "created_at" => date("2021-03-15 11:45:32")
+]);
 
-];
+$accounts = $dao->get_all_accounts();
 
-
-
-
-
-$owner = $owner_dao->add_owner($owner1);
-
-print_r($owner1);
+print_r($accounts);
 
 
  ?>
