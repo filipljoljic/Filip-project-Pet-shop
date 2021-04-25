930f614f6a7554977a0e0065810fb377bb2ee0cd
filipljoljic <filip.ljoljic@stu.ibu.edu.ba>
2021-04-25 22:57:28 +0200

    routes work

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
-echo "Hello Corona";
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require dirname(__FILE__).'../vendor/autoload.php';
+
+//die('this request was sent to the index.php file');
 
 
-echo "sad ovo";
-echo "dal radi ovo";
+Flight::route('/', function(){
+  echo 'Hello world!!!!!!!!';
+});
 
+Flight::route('/cao', function(){
+  echo 'Hello world2!';
+});
+
+Flight::start();
 
  ?>
